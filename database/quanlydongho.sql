41df505bc120df70042d030194129123e57ad4c1
TonThanhTam <tamtt1512@gmail.com>
2023-04-21 15:08:04 +0700

    add database

---
+++ database/quanlydongho.sql
@@ -0,0 +1,373 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th4 21, 2023 lúc 10:07 AM
+-- Phiên bản máy phục vụ: 10.4.17-MariaDB
+-- Phiên bản PHP: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `quanlydongho`
+--
+CREATE DATABASE IF NOT EXISTS `quanlydongho` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `quanlydongho`;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `cthoadon`
+--
+
+DROP TABLE IF EXISTS `cthoadon`;
+CREATE TABLE IF NOT EXISTS `cthoadon` (
+  `MaHD` int(11) NOT NULL,
+  `MaSP` int(11) NOT NULL,
+  `SoLuong` int(11) NOT NULL,
+  `DonGia` int(11) NOT NULL,
+  `ThanhTien` int(11) NOT NULL,
+  PRIMARY KEY (`MaHD`,`MaSP`),
+  KEY `cthoadon_ibfk_2` (`MaSP`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `cthoadon`
+--
+
+INSERT INTO `cthoadon` (`MaHD`, `MaSP`, `SoLuong`, `DonGia`, `ThanhTien`) VALUES
+(1, 1, 1, 1607000, 1607000);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `ctphieunhap`
+--
+
+DROP TABLE IF EXISTS `ctphieunhap`;
+CREATE TABLE IF NOT EXISTS `ctphieunhap` (
+  `MaPN` int(11) NOT NULL,
+  `MaSP` int(11) NOT NULL,
+  `SoLuong` int(11) NOT NULL,
+  `DonGia` int(11) NOT NULL,
+  `ThanhTien` int(11) NOT NULL,
+  PRIMARY KEY (`MaPN`,`MaSP`),
+  KEY `ctphieunhap_ibfk_2` (`MaSP`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `ctphieunhap`
+--
+
+INSERT INTO `ctphieunhap` (`MaPN`, `MaSP`, `SoLuong`, `DonGia`, `ThanhTien`) VALUES
+(1, 1, 3, 1607000, 4821000);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `giamgia`
+--
+
+DROP TABLE IF EXISTS `giamgia`;
+CREATE TABLE IF NOT EXISTS `giamgia` (
+  `MaGG` int(11) NOT NULL AUTO_INCREMENT,
+  `TenGG` text NOT NULL,
+  `PhanTramGiam` int(11) NOT NULL,
+  `DieuKien` int(11) NOT NULL,
+  `NgayBD` date NOT NULL,
+  `NgayKT` date NOT NULL,
+  PRIMARY KEY (`MaGG`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `giamgia`
+--
+
+INSERT INTO `giamgia` (`MaGG`, `TenGG`, `PhanTramGiam`, `DieuKien`, `NgayBD`, `NgayKT`) VALUES
+(1, 'Không giảm giá', 0, 0, '2023-02-26', '2028-12-31');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `hoadon`
+--
+
+DROP TABLE IF EXISTS `hoadon`;
+CREATE TABLE IF NOT EXISTS `hoadon` (
+  `MaHD` int(11) NOT NULL AUTO_INCREMENT,
+  `MaNV` int(11) NOT NULL,
+  `MaKH` int(11) NOT NULL,
+  `NgayLap` date NOT NULL,
+  `TongTien` int(11) NOT NULL,
+  `GhiChu` text NOT NULL,
+  PRIMARY KEY (`MaHD`),
+  KEY `hoadon_ibfk_1` (`MaKH`),
+  KEY `hoadon_ibfk_2` (`MaNV`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `hoadon`
+--
+
+INSERT INTO `hoadon` (`MaHD`, `MaNV`, `MaKH`, `NgayLap`, `TongTien`, `GhiChu`) VALUES
+(1, 1, 1, '2023-02-28', 1607000, 'Đã thanh toán');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `khachhang`
+--
+
+DROP TABLE IF EXISTS `khachhang`;
+CREATE TABLE IF NOT EXISTS `khachhang` (
+  `MaKH` int(11) NOT NULL AUTO_INCREMENT,
+  `Ho` varchar(255) NOT NULL,
+  `Ten` varchar(255) NOT NULL,
+  `GioiTinh` varchar(3) NOT NULL,
+  `SoDienThoai` varchar(10) NOT NULL,
+  `TongChiTieu` int(11) NOT NULL DEFAULT 0,
+  `TinhTrang` int(11) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`MaKH`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `khachhang`
+--
+
+INSERT INTO `khachhang` (`MaKH`, `Ho`, `Ten`, `GioiTinh`, `SoDienThoai`, `TongChiTieu`, `TinhTrang`) VALUES
+(1, 'Tôn Thành', 'Tâm', 'Nam', '0396265413', 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `loai`
+--
+
+DROP TABLE IF EXISTS `loai`;
+CREATE TABLE IF NOT EXISTS `loai` (
+  `MaLoai` int(11) NOT NULL AUTO_INCREMENT,
+  `TenLoai` varchar(255) NOT NULL,
+  PRIMARY KEY (`MaLoai`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `loai`
+--
+
+INSERT INTO `loai` (`MaLoai`, `TenLoai`) VALUES
+(1, 'Đồng hồ Nam');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `nhacungcap`
+--
+
+DROP TABLE IF EXISTS `nhacungcap`;
+CREATE TABLE IF NOT EXISTS `nhacungcap` (
+  `MaNCC` int(11) NOT NULL AUTO_INCREMENT,
+  `TenNCC` varchar(255) NOT NULL,
+  `DiaChi` varchar(255) NOT NULL,
+  `SoDienThoai` varchar(11) NOT NULL,
+  PRIMARY KEY (`MaNCC`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `nhacungcap`
+--
+
+INSERT INTO `nhacungcap` (`MaNCC`, `TenNCC`, `DiaChi`, `SoDienThoai`) VALUES
+(1, 'CASIO', 'Nhật Bản', '0396265413');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `nhanvien`
+--
+
+DROP TABLE IF EXISTS `nhanvien`;
+CREATE TABLE IF NOT EXISTS `nhanvien` (
+  `MaNV` int(11) NOT NULL AUTO_INCREMENT,
+  `Ho` varchar(255) DEFAULT NULL,
+  `Ten` varchar(255) DEFAULT NULL,
+  `GioiTinh` varchar(3) DEFAULT NULL,
+  `ChucVu` varchar(100) NOT NULL,
+  `Quyen` varchar(255) NOT NULL,
+  PRIMARY KEY (`MaNV`),
+  KEY `nhanvien_ibfk_1` (`Quyen`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `nhanvien`
+--
+
+INSERT INTO `nhanvien` (`MaNV`, `Ho`, `Ten`, `GioiTinh`, `ChucVu`, `Quyen`) VALUES
+(1, 'Admin', NULL, NULL, 'Admin', 'Quản trị');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `phanquyen`
+--
+
+DROP TABLE IF EXISTS `phanquyen`;
+CREATE TABLE IF NOT EXISTS `phanquyen` (
+  `Quyen` varchar(255) NOT NULL DEFAULT 'Default' COMMENT 'Tên quyền',
+  `NhapHang` int(1) NOT NULL DEFAULT 0,
+  `QLSanPham` int(1) NOT NULL DEFAULT 0,
+  `QLNhanVien` int(1) NOT NULL DEFAULT 0,
+  `QLKhachHang` int(1) NOT NULL DEFAULT 0,
+  `ThongKe` int(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`Quyen`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `phanquyen`
+--
+
+INSERT INTO `phanquyen` (`Quyen`, `NhapHang`, `QLSanPham`, `QLNhanVien`, `QLKhachHang`, `ThongKe`) VALUES
+('Default', 0, 0, 0, 0, 0),
+('Nhân viên', 0, 0, 0, 1, 0),
+('Quản lý', 1, 0, 1, 1, 1),
+('Quản trị', 1, 1, 1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `phieunhap`
+--
+
+DROP TABLE IF EXISTS `phieunhap`;
+CREATE TABLE IF NOT EXISTS `phieunhap` (
+  `MaPN` int(11) NOT NULL AUTO_INCREMENT,
+  `MaNV` int(11) NOT NULL,
+  `MaNCC` int(11) NOT NULL,
+  `NgayLap` date NOT NULL,
+  `TongTien` int(11) NOT NULL,
+  PRIMARY KEY (`MaPN`),
+  KEY `phieunhap_ibfk_1` (`MaNCC`),
+  KEY `phieunhap_ibfk_2` (`MaNV`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `phieunhap`
+--
+
+INSERT INTO `phieunhap` (`MaPN`, `MaNV`, `MaNCC`, `NgayLap`, `TongTien`) VALUES
+(1, 1, 1, '2023-02-28', 4821000);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `sanpham`
+--
+
+DROP TABLE IF EXISTS `sanpham`;
+CREATE TABLE IF NOT EXISTS `sanpham` (
+  `MaSP` int(11) NOT NULL AUTO_INCREMENT,
+  `TenSP` varchar(255) NOT NULL,
+  `MaLoai` int(11) NOT NULL,
+  `MaNCC` int(11) NOT NULL,
+  `SoLuong` int(11) NOT NULL,
+  `HinhAnh` varchar(255) NOT NULL,
+  `DonGia` int(11) NOT NULL,
+  `MoTa` text NOT NULL,
+  PRIMARY KEY (`MaSP`),
+  KEY `sp_ibfk_1` (`MaLoai`),
+  KEY `sp_ibfk_2` (`MaNCC`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `sanpham`
+--
+
+INSERT INTO `sanpham` (`MaSP`, `TenSP`, `MaLoai`, `MaNCC`, `SoLuong`, `HinhAnh`, `DonGia`, `MoTa`) VALUES
+(1, 'CASIO NAM – QUARTZ (PIN) – DÂY KIM LOẠI (MTP-1370D-7A2VDF)', 1, 1, 22, 'casio0.png', 1607000, 'Đồng hồ nam Casio MTP-1370D-7A2VDF thanh lịch với kiểu dáng nam tính mặt đồng hồ nền trắng cùng chữ số vạch mạ vàng, chất liệu được làm từ thép không gỉ, 3 kim vàng, còn có 1 lịch ngày và 1 lịch thứ');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `taikhoan`
+--
+
+DROP TABLE IF EXISTS `taikhoan`;
+CREATE TABLE IF NOT EXISTS `taikhoan` (
+  `MaNV` int(11) NOT NULL,
+  `TaiKhoan` varchar(255) NOT NULL,
+  `MatKhau` varchar(255) NOT NULL,
+  `TrangThai` int(11) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`MaNV`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `taikhoan`
+--
+
+INSERT INTO `taikhoan` (`MaNV`, `TaiKhoan`, `MatKhau`, `TrangThai`) VALUES
+(1, 'admin', 'admin', 1);
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `cthoadon`
+--
+ALTER TABLE `cthoadon`
+  ADD CONSTRAINT `cthoadon_ibfk_1` FOREIGN KEY (`MaHD`) REFERENCES `hoadon` (`MaHD`),
+  ADD CONSTRAINT `cthoadon_ibfk_2` FOREIGN KEY (`MaSP`) REFERENCES `sanpham` (`MaSP`);
+
+--
+-- Các ràng buộc cho bảng `ctphieunhap`
+--
+ALTER TABLE `ctphieunhap`
+  ADD CONSTRAINT `ctphieunhap_ibfk_1` FOREIGN KEY (`MaPN`) REFERENCES `phieunhap` (`MaPN`),
+  ADD CONSTRAINT `ctphieunhap_ibfk_2` FOREIGN KEY (`MaSP`) REFERENCES `sanpham` (`MaSP`);
+
+--
+-- Các ràng buộc cho bảng `hoadon`
+--
+ALTER TABLE `hoadon`
+  ADD CONSTRAINT `hoadon_ibfk_1` FOREIGN KEY (`MaKH`) REFERENCES `khachhang` (`MaKH`),
+  ADD CONSTRAINT `hoadon_ibfk_2` FOREIGN KEY (`MaNV`) REFERENCES `nhanvien` (`MaNV`);
+
+--
+-- Các ràng buộc cho bảng `nhanvien`
+--
+ALTER TABLE `nhanvien`
+  ADD CONSTRAINT `nhanvien_ibfk_1` FOREIGN KEY (`Quyen`) REFERENCES `phanquyen` (`Quyen`),
+  ADD CONSTRAINT `nhanvien_ibfk_2` FOREIGN KEY (`MaNV`) REFERENCES `taikhoan` (`MaNV`);
+
+--
+-- Các ràng buộc cho bảng `phieunhap`
+--
+ALTER TABLE `phieunhap`
+  ADD CONSTRAINT `phieunhap_ibfk_1` FOREIGN KEY (`MaNCC`) REFERENCES `nhacungcap` (`MaNCC`),
+  ADD CONSTRAINT `phieunhap_ibfk_2` FOREIGN KEY (`MaNV`) REFERENCES `nhanvien` (`MaNV`);
+
+--
+-- Các ràng buộc cho bảng `sanpham`
+--
+ALTER TABLE `sanpham`
+  ADD CONSTRAINT `sp_ibfk_1` FOREIGN KEY (`MaLoai`) REFERENCES `loai` (`MaLoai`),
+  ADD CONSTRAINT `sp_ibfk_2` FOREIGN KEY (`MaNCC`) REFERENCES `nhacungcap` (`MaNCC`);
+
+--
+-- Các ràng buộc cho bảng `taikhoan`
+--
+ALTER TABLE `taikhoan`
+  ADD CONSTRAINT `taikhoan_ibfk_1` FOREIGN KEY (`MaNV`) REFERENCES `nhanvien` (`maNV`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
